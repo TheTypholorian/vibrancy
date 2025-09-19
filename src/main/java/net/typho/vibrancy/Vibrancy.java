1d8889e1d5d4a9cf15aa7d7b6147165016a015e8
The Typhothanian <thetyphothanian@gmail.com>
2025-09-19 14:06:37 -0600

    why didn't these commit

--- src/main/java/net/typho/vibrancy/Vibrancy.java
+++ src/main/java/net/typho/vibrancy/Vibrancy.java
@@ -3,6 +3,8 @@ package net.typho.vibrancy;
 import net.fabricmc.api.ModInitializer;
 
 public class Vibrancy implements ModInitializer {
+    public static final String MOD_ID = "vibrancy";
+
     @Override
     public void onInitialize() {
     }
--- src/main/java/net/typho/vibrancy/client/VibrancyClient.java
+++ src/main/java/net/typho/vibrancy/client/VibrancyClient.java
@@ -1,9 +1,16 @@
 package net.typho.vibrancy.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.typho.vibrancy.Vibrancy;
 
 public class VibrancyClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of(Vibrancy.MOD_ID, "vibrancy"), FabricLoader.getInstance().getModContainer(Vibrancy.MOD_ID).orElseThrow(), Text.translatable("pack.name.vibrancy"), ResourcePackActivationType.DEFAULT_ENABLED);
     }
 }
