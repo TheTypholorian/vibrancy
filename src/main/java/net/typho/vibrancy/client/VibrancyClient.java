674ba3a75d17f14e893af459e2802237ae847c7d
The Typhothanian <thetyphothanian@gmail.com>
2025-09-19 14:06:07 -0600

    gold stuff retextured

---
+++ src/main/java/net/typho/vibrancy/client/VibrancyClient.java
@@ -0,0 +1,9 @@
+package net.typho.vibrancy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VibrancyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
