dda861a2bed91023413c50621631aac58ee0932f
The Typhothanian <thetyphothanian@gmail.com>
2025-10-25 19:46:02 -0600

    stuff idk

--- forge/src/main/java/net/typho/vibrancy/VibrancyForge.java
+++ forge/src/main/java/net/typho/vibrancy/VibrancyForge.java
@@ -10,14 +10,14 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.function.Supplier;
 
 @Mod(Vibrancy.MOD_ID)
-public class VibrancyNeoForge {
+public class VibrancyForge {
     public static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(BuiltInRegistries.PARTICLE_TYPE, Vibrancy.MOD_ID);
     public static final Supplier<SimpleParticleType> STEAM = PARTICLES.register(
             "steam",
             () -> new SimpleParticleType(false)
     );
 
-    public VibrancyNeoForge() {
+    public VibrancyForge() {
         Vibrancy.BLOCK_STATE_PREDICATE = BlockStateModelLoader::predicate;
         Vibrancy.STEAM = STEAM;
         Vibrancy.init();
--- forge/src/main/java/net/typho/vibrancy/mixin/ForgeClientChunkCacheMixin.java
+++ forge/src/main/java/net/typho/vibrancy/mixin/ForgeClientChunkCacheMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientChunkCache.class)
-public class NeoForgeClientChunkCacheMixin {
+public class ForgeClientChunkCacheMixin {
     @Inject(
             method = "updateViewRadius",
             at = @At(
