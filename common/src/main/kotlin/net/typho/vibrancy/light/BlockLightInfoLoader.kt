92a367391306dfa30b68345a11d21ded18fbf47b
The Typhothanian <thetyphothanian@gmail.com>
2026-01-10 12:46:17 -0700

    added compat for another furniture, supplementaries (partially), amendments, and wathe

--- common/src/main/kotlin/net/typho/vibrancy/light/BlockLightInfoLoader.kt
+++ common/src/main/kotlin/net/typho/vibrancy/light/BlockLightInfoLoader.kt
@@ -4,32 +4,58 @@ import com.google.gson.JsonParser
 import com.google.gson.JsonSyntaxException
 import com.mojang.serialization.JsonOps
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
 import net.minecraft.resources.FileToIdConverter
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.packs.resources.ResourceManager
+import net.minecraft.tags.TagKey
+import net.minecraft.world.level.block.Block
 import net.typho.big_shot_lib.resource.SynchronousReloadListener
 import net.typho.vibrancy.Vibrancy
+import java.io.Reader
 
 object BlockLightInfoLoader : SynchronousReloadListener {
     @JvmField
     val idConverter: FileToIdConverter = FileToIdConverter.json("block_lights")
+
+    fun load(block: Block, key: ResourceLocation, reader: Reader) {
+        BlockLightInfo.MAP.put(
+            block,
+            BlockLightInfo.codec(block.stateDefinition)
+                .codec()
+                .parse(
+                    JsonOps.INSTANCE,
+                    JsonParser.parseReader(reader)
+                )
+                .getOrThrow { message -> JsonSyntaxException("Error parsing block light info for $key: $message") }
+        )
+    }
 
     override fun reload(manager: ResourceManager) {
         BlockLightInfo.MAP.clear()
 
         for (entry in idConverter.listMatchingResources(manager)) {
             entry.value.openAsReader().use { jsonReader ->
-                val blockKey = idConverter.fileToId(entry.key)
-                val block = BuiltInRegistries.BLOCK.get(blockKey)
-                BlockLightInfo.MAP.put(
-                    block,
-                    BlockLightInfo.codec(block.stateDefinition)
-                        .codec()
-                        .parse(
-                            JsonOps.INSTANCE,
-                            JsonParser.parseReader(jsonReader)
-                        )
-                        .getOrThrow { message -> JsonSyntaxException("Error parsing block light info for $blockKey: $message") }
-                )
+                var blockKey = idConverter.fileToId(entry.key)
+
+                if (blockKey.path.startsWith("tag/")) {
+                    blockKey = blockKey.withPath { path -> path.substring("tag/".length) }
+
+                    BuiltInRegistries.BLOCK.getTag(TagKey.create(Registries.BLOCK, blockKey))
+                        .ifPresent { tag ->
+                            tag.forEach { block ->
+                                load(
+                                    block.value(),
+                                    block.unwrapKey()
+                                        .map { key -> key.location() }
+                                        .orElse(blockKey),
+                                    jsonReader
+                                )
+                            }
+                        }
+                } else if (BuiltInRegistries.BLOCK.containsKey(blockKey)) {
+                    load(BuiltInRegistries.BLOCK.get(blockKey), blockKey, jsonReader)
+                }
             }
         }
 
