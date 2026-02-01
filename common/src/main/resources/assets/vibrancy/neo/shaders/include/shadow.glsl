c15e15bca61cc5fb125214888f503502795d025a
The Typhothanian <thetyphothanian@gmail.com>
2026-02-01 10:10:44 -0700

    improved shadow texture mapping

--- common/src/main/resources/assets/vibrancy/neo/shaders/include/shadow.glsl
+++ common/src/main/resources/assets/vibrancy/neo/shaders/include/shadow.glsl
@@ -1,13 +1,14 @@
 vec2 directionToShadowCoords(vec3 dir) {
-    vec2 uv = vec2(atan(dir.z, dir.x), asin(dir.y)) / 3.1415926535;
-    uv.x = (uv.x + 1) / 2;
-    uv.y += 0.5;
+    vec2 uv = vec2(atan(dir.z, dir.x) / 3.1415926535, dir.y);
+    uv.x = (uv.x + 1.0) * 0.5;
+    uv.y = uv.y * 0.5 + 0.5;
     return uv;
 }
 
 vec3 shadowCoordsToDirection(vec2 uv) {
-    vec2 ang = vec2(uv.x * 2.0 - 1.0, uv.y - 0.5) * 3.1415926535;
-    float cosAng = cos(ang.y);
-    vec3 dir = vec3(cos(ang.x) * cosAng, sin(ang.y), sin(ang.x) * cosAng);
+    float angX = (uv.x * 2.0 - 1.0) * 3.1415926535;
+    float angY = uv.y * 2.0 - 1.0;
+    float cosAng = sqrt(1.0 - angY * angY);
+    vec3 dir = vec3(cos(angX) * cosAng, angY, sin(angX) * cosAng);
     return dir;
 }
