c539bbbeb84b324527f55c112818fa458557a1b5
The Typhothanian <thetyphothanian@gmail.com>
2026-02-07 10:11:00 -0700

    more things I forgot to do

--- common/src/main/kotlin/net/typho/vibrancy/LightManager.kt
+++ common/src/main/kotlin/net/typho/vibrancy/LightManager.kt
@@ -23,7 +23,7 @@ import net.typho.big_shot_lib.gl.state.DepthMask
 import net.typho.big_shot_lib.gl.state.GlCapability
 import net.typho.vibrancy.block.*
 import net.typho.vibrancy.mixin.LevelRendererAccessor
-import net.typho.vibrancy.shadows.ShadowGreedyMesher
+import net.typho.vibrancy.shadows.BasicShadowMesher
 import net.typho.vibrancy.shadows.ShadowMesher
 import net.typho.vibrancy.sky.SkyLight
 import net.typho.vibrancy.sky.SkyLightType
@@ -77,11 +77,8 @@ open class LightManager {
 
     fun getCullingFrustum(): Frustum = (Minecraft.getInstance().levelRenderer as LevelRendererAccessor).cullingFrustum
 
-    fun createShadowMesher(light: PointLight): ShadowMesher? {
-        return light.getShadowBox()?.let { box ->
-            // if (Vibrancy.config.forNerds.useGreedyMeshing.get()) ShadowGreedyMesher(box) else
-            ShadowGreedyMesher(box)
-        }
+    fun createShadowMesher(light: PointLight): ShadowMesher {
+        return BasicShadowMesher()
     }
 
     fun rebuildAllShadows() {
