76d30fd8c79c8cd643d5c11ad3c0012abc23a11c
The Typhothanian <thetyphothanian@gmail.com>
2026-02-12 08:10:05 -0700

    removed debugging code

--- common/src/main/kotlin/net/typho/vibrancy/shadows/AsyncBlockShadowTexture.kt
+++ common/src/main/kotlin/net/typho/vibrancy/shadows/AsyncBlockShadowTexture.kt
@@ -34,7 +34,7 @@ open class AsyncBlockShadowTexture(
 
                 builder.finish()
 
-                //asyncTask = null
+                asyncTask = null
                 return true
             }
         }
