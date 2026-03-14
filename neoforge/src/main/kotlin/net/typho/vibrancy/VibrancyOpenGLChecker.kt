33d8e4259a652d8e58a5c13b521864aa2e9e0654
The Typhothanian <thetyphothanian@gmail.com>
2026-03-13 20:51:47 -0600

    added ssbo checker

---
+++ neoforge/src/main/kotlin/net/typho/vibrancy/VibrancyOpenGLChecker.kt
@@ -0,0 +1,29 @@
+package net.typho.vibrancy
+
+import net.neoforged.api.distmarker.Dist
+import net.neoforged.bus.api.IEventBus
+import net.neoforged.fml.ModContainer
+import net.neoforged.fml.ModLoadingException
+import net.neoforged.fml.ModLoadingIssue
+import net.neoforged.fml.common.Mod
+import net.typho.big_shot_lib.api.client.opengl.util.OpenGL
+import org.lwjgl.opengl.GL
+import org.lwjgl.system.Platform
+
+@Mod(value = Vibrancy.MOD_ID, dist = [Dist.CLIENT])
+class VibrancyOpenGLChecker(eventBus: IEventBus, modContainer: ModContainer) {
+    init {
+        OpenGL.INSTANCE.recordRenderCall {
+            if (!GL.getCapabilities().GL_ARB_shader_storage_buffer_object) {
+                throw ModLoadingException(
+                    ModLoadingIssue.error(
+                        if (Platform.get() == Platform.MACOSX)
+                            "Vibrancy requires GL_ARB_shader_storage_buffer_object (OpenGL 4.3), which MacOS does not support, and there is no way to fix it."
+                        else
+                            "Vibrancy requires GL_ARB_shader_storage_buffer_object (OpenGL 4.3), which your system does not support. This can typically be solved by updating your GPU drivers."
+                    )
+                )
+            }
+        }
+    }
+}
