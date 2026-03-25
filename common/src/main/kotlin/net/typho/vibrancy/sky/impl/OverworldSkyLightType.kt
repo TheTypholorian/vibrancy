3edbce41f35d5a7bfd69ea44f3097598bd9b9c4e
The Typhothanian <thetyphothanian@gmail.com>
2026-03-25 07:21:15 -0600

    removed sky light blit interval

--- common/src/main/kotlin/net/typho/vibrancy/sky/impl/OverworldSkyLightType.kt
+++ common/src/main/kotlin/net/typho/vibrancy/sky/impl/OverworldSkyLightType.kt
@@ -19,8 +19,6 @@ object OverworldSkyLightType : SkyLightType<OverworldSkyLightInfo, OverworldSkyL
         return info as? OverworldSkyLightInfo
     }
 
-    var lastBlit: Long = 0
-
     override fun render(
         manager: LightManager,
         data: RenderEventData,
@@ -28,15 +26,10 @@ object OverworldSkyLightType : SkyLightType<OverworldSkyLightInfo, OverworldSkyL
         fbo: GlFramebuffer,
         debugOut: (String, Int) -> Unit
     ) {
-        val time = System.currentTimeMillis()
-
-        if (time - lastBlit >= 100) { // TODO
-            lastBlit = time
-            val blitSettings = OverworldSkyLightStorage.meshBlitSettings(data, lights)
-            blitSettings.bind()
-            lights.chunks.values.forEach { it.updateShadows(manager, data, debugOut) }
-            blitSettings.unbind()
-        }
+        val blitSettings = OverworldSkyLightStorage.meshBlitSettings(data, lights)
+        blitSettings.bind()
+        lights.chunks.values.forEach { it.updateShadows(manager, data, debugOut) }
+        blitSettings.unbind()
 
         val settings = LightMesh.renderSettings(fbo, data, TextureUtil.INSTANCE.blockAtlas)
         val shader = NeoShaderRegistry.get(Vibrancy.id("light_mesh"))!! // TODO
