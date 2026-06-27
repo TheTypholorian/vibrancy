f00c4d402b71b412b19f9999b170ca97fb0f87a5
The Typhothanian <thetyphothanian@gmail.com>
2026-06-26 18:21:43 -0600

    block light texture yay

---
+++ src/main/kotlin/net/typho/vibrancy/BlockLightTexture.kt
@@ -0,0 +1,22 @@
+package net.typho.vibrancy
+
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.LightTexture
+import net.minecraft.world.level.dimension.DimensionType
+
+object BlockLightTexture : LightTexture(Minecraft.getInstance().gameRenderer, Minecraft.getInstance()) {
+    @JvmField
+    val inUse = ThreadLocal<Boolean>()
+
+    @JvmStatic
+    fun isInUse() = inUse.get() ?: false
+
+    fun getSkyBrightness(dimension: DimensionType, x: Int): Float {
+        return getBrightness(dimension, x)
+    }
+
+    fun getBlockBrightness(dimension: DimensionType, x: Int): Float {
+        val f = getBrightness(dimension, x)
+        return f//.pow(0.75f) * 0.75f
+    }
+}
